a100eb1c18be8b59fb85153e3d77bac6d9c50224
陶开 <taok@shicydeMacBook-Pro.local>
2021-06-22 14:19:21 +0800

    add utils_ktao/misfit/pdf_merge.sh

--- utils_ktao/misfit/merge_figure.sh
+++ utils_ktao/misfit/merge_figure.sh
@@ -6,6 +6,8 @@ misfit_list=${1:?[arg]need misfit list (e.g. misfit.txt)}
 figure_dir=${2:?[arg]need figure dir}
 out_dir=${3:?[arg]need out_dir}
 
+exec_dir=$(dirname $(readlink -f $0))
+
 for win in $(awk '$1!~/#/{print $2}' $misfit_list | sort -u)
 do
   echo ====== $win
@@ -17,6 +19,6 @@ do
     echo "[ERROR] cannot find input figures"
   else
     rm $output_fig
-    pdf_merge.sh $output_fig $input_figs
+    ${exec_dir}/pdf_merge.sh $output_fig $input_figs
   fi
 done
